57fef1835d5b5b54f41289b8f26c53b0bb385fd4
GT-Zhang <1029550448@qq.com>
2021-06-18 03:33:37 +0800

    Release V1.0a1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 # python setup.py sdist bdist_wheel
 setup(
     name='QPT',
-    version='1.0a0',
+    version='1.0a1',
     packages=find_packages(),
     url='https://github.com/GT-ZhangAcer/QPT',
     license='LGPL',

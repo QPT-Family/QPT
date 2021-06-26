72c1d062b78c01d5399f81c2a89da6fbbde7b07b
GT-Zhang <1029550448@qq.com>
2021-06-26 17:29:04 +0800

    1.0a8 - 修复依赖打包时的提示

--- unit_test/sandbox_m/run.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
-
-app = QApplication(sys.argv)
-widget = QWidget()
-msg = QMessageBox.information(widget, '测试结果', '测试成功',
-                              QMessageBox.Yes)
-widget.close()

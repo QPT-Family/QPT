7c158ad8f54789d7d080f77484d98d46d0247db9
GT-Zhang <1029550448@qq.com>
2021-06-26 22:34:32 +0800

    1.0a8 - add单测文件

---
+++ unit_test/sandbox_m/run.py
@@ -0,0 +1,18 @@
+# Author: Acer Zhang
+# Datetime:2021/6/26 
+# Copyright belongs to the author.
+# Please indicate the source for reprinting.
+
+import sys
+
+# from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+#
+# app = QApplication(sys.argv)
+# widget = QWidget()
+# msg = QMessageBox.information(widget, '测试结果', '测试成功',
+#                               QMessageBox.Yes)
+# widget.close()
+import tkinter
+top = tkinter.Tk()
+# 进入消息循环
+top.mainloop()

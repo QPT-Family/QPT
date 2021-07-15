8b2538dc32601a8311672f9eda21b632403d5214
GT-Zhang <46156734+GT-ZhangAcer@users.noreply.github.com>
2021-07-15 13:21:07 +0800

    Update entry_run.cmd

--- qpt/ext/launcher_entry/entry_run.cmd
+++ qpt/ext/launcher_entry/entry_run.cmd
@@ -2,5 +2,5 @@ set QPT_COLOR=False
 set QPT_MODE=Run
 echo on
 cls
-"./Python/python.exe" -c "import sys;sys.path.append('./Python');sys.path.append('./Python/Lib/site-packages');sys.path.append('./Python/Scripts');import qpt.run as run"
+"./Python/python.exe" -c "import sys;sys.path.append('./Python');sys.path.append('./Python/Lib');sys.path.append('./Python/Lib/site-packages');sys.path.append('./Python/Scripts');import qpt.run as run"
 pause

0315b2d273534d693dcd4f2f2a6bb53df55e5a32
GT-Zhang <46156734+GT-ZhangAcer@users.noreply.github.com>
2021-10-19 17:55:42 +0800

    Update entry_run_hidden.cmd

--- qpt/ext/launcher_entry/entry_run_hidden.cmd
+++ qpt/ext/launcher_entry/entry_run_hidden.cmd
@@ -5,4 +5,4 @@ set QPT_COLOR=False
 set QPT_MODE=Run
 cls
 echo off
-"./Python/pythonw.exe" -c "import sys;sys.path.append('./Python');sys.path.append('./Python/Lib');sys.path.append('./Python/Lib/site-packages');sys.path.append('./Python/Scripts');import qpt.run as run"
+"./Python/python.exe" -c "import sys;sys.path.append('./Python');sys.path.append('./Python/Lib');sys.path.append('./Python/Lib/site-packages');sys.path.append('./Python/Scripts');import qpt.run as run"

f6a86c27548052863470af775c88513949ff5f5a
GT-Zhang <1029550448@qq.com>
2022-03-24 15:36:53 +0800

    Update entry_run_hidden.cmd

--- qpt/ext/launcher_entry/entry_run_hidden.cmd
+++ qpt/ext/launcher_entry/entry_run_hidden.cmd
@@ -5,6 +5,6 @@ set QPT_COLOR=False
 set QPT_MODE=Run
 cls
 set PYTHONPATH=Python/Lib/site-packages;Python/Lib;Python
-set PATH=Python/Lib/site-package;Python/Lib;Python;%PATH%
+set PATH=Python/Lib/site-package;Python/Lib;Python/Scripts;Python;%PATH%
 echo off
 "./Python/python.exe" -c "import sys;sys.path.append('./Python');sys.path.append('./Python/Lib');sys.path.append('./Python/Lib/site-packages');sys.path.append('./Python/Scripts');import qpt.run as run"

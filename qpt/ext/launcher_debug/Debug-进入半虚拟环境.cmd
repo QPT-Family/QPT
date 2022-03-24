c3819ed88919416ccd82ab33f1a9a1ed211725a1
GT-Zhang <1029550448@qq.com>
2022-03-24 15:36:01 +0800

    Update Debug-进入半虚拟环境.cmd

--- qpt/ext/launcher_debug/Debug-进入半虚拟环境.cmd
+++ qpt/ext/launcher_debug/Debug-进入半虚拟环境.cmd
@@ -4,7 +4,7 @@ cd /d %~dp0
 set QPT_COLOR=False
 set QPT_MODE=Debug
 set PYTHONPATH=Python/Lib/site-packages;Python/Lib;Python
-set PATH=Python/Lib/site-package;Python/Lib;Python;%PATH%
+set PATH=Python/Lib/site-package;Python/Lib;Python/Scripts;Python;%PATH%
 set PROMPT=(QPT_VENV) %PROMPT%
 cls
 echo QPT successful!

1b584626e0cc59b77e85ed3b29b9adf0a062fe19
GT-Zhang <1029550448@qq.com>
2022-03-24 15:36:31 +0800

    Update entry_debug.cmd

--- qpt/ext/launcher_entry/entry_debug.cmd
+++ qpt/ext/launcher_entry/entry_debug.cmd
@@ -5,7 +5,7 @@ cd ..
 set QPT_COLOR=False
 set QPT_MODE=Debug
 set PYTHONPATH=Python/Lib/site-packages;Python/Lib;Python
-set PATH=Python/Lib/site-package;Python/Lib;Python;%PATH%
+set PATH=Python/Lib/site-package;Python/Lib;Python/Scripts;Python;%PATH%
 cls
 "./Python/python.exe" -c "import sys;sys.path.append('./Python');sys.path.append('./Python/Lib');sys.path.append('./Python/Lib/site-packages');sys.path.append('./Python/Scripts');import qpt.run as run"
 pause

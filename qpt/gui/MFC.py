d3aaf7144e7afd3df3a03d77c5cb4516db37b55b
GT-Zhang <1029550448@qq.com>
2023-10-29 22:17:09 +0800

    画个饼先

---
+++ qpt/gui/MFC.py
@@ -0,0 +1,7 @@
+# Author: Acer Zhang
+# Datetime: 2023/10/29
+# Copyright belongs to the author.
+# Please indicate the source for reprinting.
+
+# https://github.com/mhammond/pywin32/blob/main/Pythonwin/pywin/Demos/progressbar.py
+# 暂时不做，因为pywin32的体积不亚于Tkinter，取代Tkinter
